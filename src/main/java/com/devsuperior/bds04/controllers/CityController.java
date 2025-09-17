7cc11a66a56e9dd71cf2d97d0d01e035ab9f0e60
conradrenno <conradrenno@hotmail.com>
2025-09-17 15:13:42 -0300

    Adding CityController + findAll method

---
+++ src/main/java/com/devsuperior/bds04/controllers/CityController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.bds04.controllers;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+
+    @Autowired
+    private CityService cityService;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        List<CityDTO> list = cityService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
---
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    public List<CityDTO> findAll() {
+        List<City> list = cityRepository.findAll(Sort.by("name"));
+        return list.stream().map(x -> new CityDTO(x)).toList();
+    }
+
+}
+
+
+
